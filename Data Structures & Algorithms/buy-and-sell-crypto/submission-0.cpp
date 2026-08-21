93055e6e3ca3e23f663280ba614b829780a93fb3
Kunal kakati <kunalkakati2003@gmail.com>
2026-08-21 16:54:54 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int res = 0;
+        int profit = 0;
+        for(int i=0; i<prices.size(); i++){
+            for(int j = i+1; j<prices.size(); j++){
+                profit = prices[j] - prices[i];
+                if(profit > res){
+                    res = profit;
+                }
+            }
+        }
+        return res;
+    }
+};
